4a1393dfb4d28c9cf85e7af53ca3be1650bbf930
Kanishak Kapur <55342602+kanishakkapur96@users.noreply.github.com>
2022-01-12 08:59:56 -0500

    Time: 3 ms (14.47%), Space: 38.7 MB (22.25%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n =  nums.length;
+        int last = nums[n-1];
+        int first = nums[0];
+        
+        // check element less than smallest in array
+        if(n<nums[0]){
+            return n;
+        }
+        for(int i=first;i<=last;i++){
+            // System.out.println(i);
+            // System.out.println(binarySearch(nums,i));
+            int count =  n - (binarySearch(nums,i));
+            if(count == i){
+                return count;
+            }
+        }
+        
+        return -1;
+        
+        
+    }
+    
+    // Returns first index of element else -1
+    int binarySearch(int[] nums,int target){
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                end = mid-1;
+            }
+            else if(nums[mid]>target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        if(ans == -1) return start;
+        return ans;
+    }
+}
