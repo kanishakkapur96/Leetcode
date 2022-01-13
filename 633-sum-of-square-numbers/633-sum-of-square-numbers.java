1b5ea176c18e69670d80fa7c8db66b86f3024baa
Kanishak Kapur <55342602+kanishakkapur96@users.noreply.github.com>
2022-01-13 07:28:28 -0500

    Time: 266 ms (5.57%), Space: 37.7 MB (28.03%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        return judgeHash(c);
+        return judgeBinary(c);
     }
     
-    // Iteration 
+    // Iteration O(sqrt(c)*log c) : Log c for sqrt operation
     boolean judgeIteration(int c){
         int limit = (int)Math.sqrt(c);
         for(int a = 0;a<=limit;a++){
@@ -15,8 +15,7 @@ class Solution {
         return false;
     }
     
-    // Hashset
-    
+    // Hashset O(sqrt(c))
     boolean judgeHash(int c){
         HashSet<Integer> sqrSet = new HashSet<>();
         for(int i=0;i<=Math.sqrt(c);i++){
@@ -30,6 +29,48 @@ class Solution {
         }
         return false;
         
+    }
+    
+    // 2 pointer approach O(sqrt(c))
+    boolean judgePointer(int c){
+        int left = 0, right = (int)Math.sqrt(c);
+        while(left<=right){
+            long sum = left*left + right*right;
+            if(sum == c){
+                return true;
+            }else if(sum<c){
+                left ++;
+            }else{
+                right--;
+            }
+        }
+        return false;
+       
+    }
+    
+    // Binary search
+    boolean judgeBinary(int c){
+       int limit = (int)Math.sqrt(c);
+       for(int a =0;a<=limit;a++){
+           int b = c - (int)(a*a);
+           if(binarySearch(0,b,b))
+               return true;
+       }
+       return false;
+    }
+    
+    boolean binarySearch(long start,long end, int  target){
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            if(mid*mid == target){
+                return true;
+            }else if(mid*mid<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return false;
     }
     
 }
