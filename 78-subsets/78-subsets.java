f0ac30a652c63e2ea7788db54d3d059672092e21
Kanishak Kapur <55342602+kanishakkapur96@users.noreply.github.com>
2022-01-16 10:40:07 -0500

    Time: 2 ms (33.40%), Space: 40.2 MB (19.50%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -5,11 +5,27 @@ class Solution {
         return result;
     }
     void findSubsets(int[] nums,int index, List<Integer> ds, List<List<Integer>> result){
+        
+        if(index == nums.length){
+            result.add(new ArrayList<Integer>(ds));
+            return;
+        }
+        
+        //Pick element
+        ds.add(nums[index]);
+        findSubsets(nums,index+1,ds,result);
+        
+        //Not pick
+        ds.remove(ds.size()-1);
+        findSubsets(nums,index+1,ds,result);
+        
+        /*
         result.add(new ArrayList<>(ds));
         for(int i = index;i<nums.length;i++){
             ds.add(nums[i]);
             findSubsets(nums,i+1,ds,result);
             ds.remove(ds.size()-1);
         }
+        */
     }
 }
