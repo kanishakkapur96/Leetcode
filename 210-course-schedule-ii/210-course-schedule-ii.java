dbcb6a5cd674f4ec013238836ea44f827155083b
Kanishak Kapur <55342602+kanishakkapur96@users.noreply.github.com>
2022-01-31 23:17:52 -0500

    Time: 19 ms (19.95%), Space: 49.5 MB (6.62%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,58 +1,52 @@
 class Solution {
-    boolean containsCycle = false;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        return topologicalOrder(numCourses,prerequisites);
-    }
-    
-     int[] topologicalOrder(int num, int[][] pre){
-        int[] visited = new int[num];
-        int[] dfsVisited = new int[num];
+        
+        // Create Hashmap
         Map<Integer,List<Integer>> map = new HashMap<>();
-        for(int[] edge : pre){
+        for(int[] edge : prerequisites){
             if(!map.containsKey(edge[1])){
-                map.put(edge[1],new ArrayList<Integer>());
+                map.put(edge[1],new ArrayList<>());
             }
             map.get(edge[1]).add(edge[0]);
         }
         
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<num;i++){
-            if(containsCycle){
-                return new int[0];
-            }
-               
-            if(visited[i] == 0){
-                topologicalSort(i,map,visited,dfsVisited,stack);
-            }
-        }
-         
         
-        int[] res = new int[stack.size()];
-        int len = stack.size();
-        for(int i=0;i<len;i++){
-            int r = stack.pop();
-            res[i] = r;
+        // Create Indegree array
+        int[] topo = new int[numCourses];
+        int[] inDeg = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+          if(map.containsKey(i)){
+            for(Integer point : map.get(i)){
+                inDeg[point]++;
+            }
+          }
         }
         
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(inDeg[i] == 0){
+                q.offer(i);
+            }
+        }
         
-        return res;
-    }
-    
-    void topologicalSort(int node, Map<Integer,List<Integer>> pre, int[] visited, int[] dfsVisited, Stack<Integer> stack){
-        visited[node] = 1;
-        dfsVisited[node] = 1;
-        if(pre.containsKey(node)){
-            for(int i: pre.get(node)){
-                if(visited[i] == 0){
-                    topologicalSort(i,pre,visited,dfsVisited,stack);
-                }else if(dfsVisited[i] == 1){
-                    containsCycle = true;
-                    return;
+        int index = 0;
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            topo[index++] = node;
+            if(map.containsKey(node)){
+             for(Integer it : map.get(node)){
+                inDeg[it] --;
+                if(inDeg[it] == 0){
+                    q.offer(it);
                 }
+             }
             }
+            
         }
         
-        dfsVisited[node] = 0;
-        stack.push(node);
+        if(index != numCourses) return new int[0];
+        
+        return topo;
+        
     }
 }
