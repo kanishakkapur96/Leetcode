4914177be805ac7441dceea64a97cd0231c06eab
Kanishak Kapur <55342602+kanishakkapur96@users.noreply.github.com>
2022-03-07 02:28:30 -0500

    Time: 5 ms (18.59%), Space: 42.6 MB (13.47%) - LeetHub

--- 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
@@ -1,16 +1,13 @@
 class Solution {
     public String removeVowels(String s) {
-        char[] alphabets = s.toCharArray();
-        for(int i=0;i<alphabets.length;i++){
-            char c = alphabets[i];
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-                alphabets[i] = '#';
+        String result = "";
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')){
+                result = result+ s.charAt(i);
             }
         }
-        String result = "";
-        for(int i=0;i<alphabets.length;i++){
-            if(alphabets[i]!='#') result = result+ alphabets[i];
-        }
+       
         return result;
     }
 }
