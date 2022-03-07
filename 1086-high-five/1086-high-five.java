0410506a5bd9fa66c891b6cde8782f2d6cb1d0e3
Kanishak Kapur <55342602+kanishakkapur96@users.noreply.github.com>
2022-03-07 10:57:50 -0500

    Time: 6 ms (74.97%), Space: 46.4 MB (16.58%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,28 +1,29 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        HashMap<Integer, PriorityQueue<Integer>> scoreMap = new HashMap<>();
+        TreeMap<Integer, PriorityQueue<Integer>> scoreMap = new TreeMap<>();
         for(int[] score : items){
-            if(!scoreMap.containsKey(score[0])) scoreMap.put(score[0],new PriorityQueue<>(Collections.reverseOrder()));
-            
-            scoreMap.get(score[0]).offer(score[1]);
+            int key = score[0];
+            int value = score[1];
+            if(!scoreMap.containsKey(key)) scoreMap.put(key,new PriorityQueue<>());
+            if(scoreMap.get(key).size()<5){
+              scoreMap.get(key).offer(value);
+            }
+            else if(value>scoreMap.get(key).peek()){
+                scoreMap.get(key).poll();
+                scoreMap.get(key).offer(value);
+            }
         }
         int[][] result = new int[scoreMap.keySet().size()][2];
         int counter = 0;
         for(int k: scoreMap.keySet()){
             int sum = 0;
-            for(int i=0;i<5;i++){
-                sum += scoreMap.get(k).poll();
+            for(Integer num : scoreMap.get(k)){
+                sum += num;
             }
             result[counter][0] = k;
             result[counter][1] = sum/5;
             counter++;
         }
-        
-        Arrays.sort(result,(a,b)-> {
-            if(a[0]<b[0]) return -1;
-            if(a[0]==b[0]) return 0;
-            return 1;
-        });
             
         return result;
     }
